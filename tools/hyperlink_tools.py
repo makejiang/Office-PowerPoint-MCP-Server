ed4b1891cf3f543e045e392d27cb5526db58885a
Kejiang Ma <kj.ma@intel.com>
2025-08-29 12:51:46 +0800

    Remove decorator from manage_hyperlinks function

--- tools/hyperlink_tools.py
+++ tools/hyperlink_tools.py
@@ -9,7 +9,6 @@ def register_hyperlink_tools(app, presentations, get_current_presentation_id, va
                           is_positive, is_non_negative, is_in_range, is_valid_rgb):
     """Register hyperlink management tools with the FastMCP app."""
     
-    @app.tool()
     def manage_hyperlinks(
         operation: str,
         slide_index: int,
